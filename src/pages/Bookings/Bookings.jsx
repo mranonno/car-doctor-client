7e0fb3afec27a47b4c6b205f366e2d77f91db18a
mranonno <anonnodas97@gmail.com>
2024-05-07 20:56:07 +0600

    set cookie and instal axios

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import axios from "axios";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
@@ -8,9 +9,15 @@ const Bookings = () => {
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setBookings(data))
+        //Call API using Axios  
+        axios.get(url)
+            .then(res => {
+            setBookings(res.data)
+            })
+        //Normal API Calling
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => setBookings(data))
     }, [url]);
 
     const handleDelete = id => {
